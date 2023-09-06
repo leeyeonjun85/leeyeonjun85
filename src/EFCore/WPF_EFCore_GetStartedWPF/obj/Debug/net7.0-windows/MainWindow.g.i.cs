8935e9b0dcd35e1c606d92f323d4036cea4fec86
leeyeonjun85 <leeyeonjun85@gmail.com>
2023-09-06 20:17:56 +0900

    a

--- src/EFCore/WPF_EFCore_GetStartedWPF/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ src/EFCore/WPF_EFCore_GetStartedWPF/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace WPF_EFCore_GetStartedWPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF_EFCore_GetStartedWPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF_EFCore_GetStartedWPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
