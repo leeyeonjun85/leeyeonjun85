b374f750aa25b94e37531c77f2ba3ae4c960ed79
leeyeonjun85 <leeyeonjun85@gmail.com>
2023-12-26 15:08:09 +0900

    a

---
+++ src/leeyeonjun/JsonBase/Models/PizzaTopping.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace JsonBase.Models
+{
+    public class PizzaTopping
+    {
+        public int PizzaId { get; set; }
+        public int ToppingId { get; set; }
+    }
+}
---
+++ src/leeyeonjun/JsonBase/Models/Sauce.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JsonBase.Models
+{
+    public class Sauce
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        public bool IsVegan { get; set; }
+    }
+}
