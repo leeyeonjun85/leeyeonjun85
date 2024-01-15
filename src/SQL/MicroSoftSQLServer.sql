789508868d905ba2a0236dbc582f9d814ca0ecb1
leeyeonjun <leeyeonjun@gmail.com>
2024-01-15 16:04:50 +0900

    Update

--- src/SQL/MicroSoftSQLServer.sql
+++ src/SQL/MicroSoftSQLServer.sql
@@ -6,8 +6,8 @@
 -- DROP TABLE leeyeonjun.dbo.Table_1;
 
 CREATE TABLE leeyeonjun.dbo.Table_1 (
-	test1 varchar(50) COLLATE Korean_Wansung_CI_AS NOT NULL,
-	test2 int NOT NULL
+	name varchar(50) COLLATE Korean_Wansung_CI_AS NOT NULL,
+	old int NOT NULL
 );
 
 
@@ -20,8 +20,8 @@ FROM sys.tables;
 SELECT *
 FROM leeyeonjun.dbo.Table_1;
 
-INSERT INTO leeyeonjun.dbo.Table_1 (test1, test2)
-VALUES('이연준', 39);
+INSERT INTO leeyeonjun.dbo.Table_1 (name, old) VALUES('이연준', 39);
+INSERT INTO leeyeonjun.dbo.Table_1 (name, old) VALUES('윤석렬', 59);
 
 
 CREATE PROCEDURE SelectAllTable_1
@@ -36,12 +36,48 @@ EXEC SelectAllTable_1;
 
 
 
-CREATE PROCEDURE SelectAll
-	@P_TableName NVARCHAR(500)
+CREATE PROCEDURE SelectAllName
+	@Column_Name NVARCHAR(50)
 AS
 	SELECT *
-	FROM @P_TableName
-GO;
+	FROM leeyeonjun.dbo.Table_1
+	WHERE name = @Column_Name
+;
+DROP PROCEDURE SelectAllName;
 
-EXEC SelectAllCustomers1 @P_TableName = 'leeyeonjun.dbo.Table_1';
+EXEC SelectAllName @Column_Name = '이연준';
+
+
+
+SELECT
+	FLOOR(RAND()*(6)+1) AS '1부터 6까지 랜덤 정수값',
+	FLOOR(RAND()*(100)+1) AS '1부터 100까지 랜덤 정수값';
+
+
+SELECT FLOOR(RAND()*10);
+
+
+DECLARE @max INT SET @max = 3
+DECLARE @min INT SET @min = 1
+SELECT CAST(((@max + 1) - @min) * RAND() + @min AS INT) AS '랜덤정수';
+
+
+DECLARE @max INT SET @max = 3
+DECLARE @min INT SET @min = 1
+SELECT CONVERT(INT, ((@max + 1) - @min) * RAND()  + @min) AS '랜덤정수';
+
+
+CREATE TABLE leeyeonjun.dbo.Table_int (IntNumber int NOT NULL)
+SELECT * FROM leeyeonjun.dbo.Table_int;
+DROP TABLE leeyeonjun.dbo.Table_int;
+
+-- 반복문에 사용될 변수 선언
+DECLARE @max INT SET @max = 10
+DECLARE @idx INT SET @idx = 1
+WHILE (@idx < @max)
+BEGIN
+    INSERT INTO leeyeonjun.dbo.Table_int (IntNumber) values (@idx)
+    SET @idx = @idx + 1
+END
+SELECT * FROM leeyeonjun.dbo.Table_int;
 
